cc3a0fad181a1e59740cfcdd814607c6361b916c
mansour1369 <75639585+mansour1369@users.noreply.github.com>
2023-03-12 23:59:06 +0330

    Add files via upload

---
+++ variance/ARM-MDK/Objects/main.d
@@ -0,0 +1,2 @@
+./objects/main.o: ..\Src\main.c \
+  E:\a\kiel\ARM\ARMCLANG\Bin\..\include\stdio.h ..\Inc\statstics.h
---
+++ variance/ARM-MDK/Objects/statstics.d
@@ -0,0 +1,2 @@
+./objects/statstics.o: ..\Src\statstics.c \
+  E:\a\kiel\ARM\ARMCLANG\Bin\..\include\math.h ..\Inc\statstics.h
